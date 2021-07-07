8d99a837e46146bdaf7d07a8803a1cc8b50ce41d
jignesh patel <jignesh.282@gmail.com>
2021-07-07 21:07:57 +1000

    implement arnie quote function

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -3,6 +3,25 @@ const { httpGet } = require('./mock-http-interface');
 const getArnieQuotes = async (urls) => {
   // TODO: Implement this function.
   // return results;
+  let quotes = [];
+
+  const all  = urls.map(item => httpGet(item));
+
+  const result = await Promise.all(all)
+
+  result.forEach(item => {
+    
+    const result = JSON.parse(item.body);
+
+    let output = {};
+    
+    output[item['status'] === 200 ? 'Arnie Quote' : 'FAILURE'] = result.message;
+
+    quotes.push(output);
+
+  });
+  
+  return quotes;
 };
 
 module.exports = {
